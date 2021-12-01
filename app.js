8968341b8f6cf59111fc1d54f1e4b2c1563b6e38
Manning <farjad_uk@hotmail.com>
2021-12-01 04:48:31 -0500

    fetch request for data

--- app.js
+++ app.js
@@ -22,3 +22,30 @@ buttonEl.addEventListener("click", function(){
 });
 
 
+//when button is clicked, run API fetch to gather data passing through the city from the event listener
+var getWeatherData = function(city){
+    var apiUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' +inputValueEl.value.trim()+ '&units=metric&appid=ad9c3547a99abdff7cc27c4cfd8394ae';
+    
+    //fetch request
+    fetch(apiUrl).then(function(response){
+        //if the response is good, parse it to JSON, and send the data through to 2 separate functions. 1 for main card, the other for the 5 day forecast
+        if(response.ok) {
+            response.json().then(function(data){
+                //variables for future display
+                var currLocation = data['name'];
+                var currTemp = data['main']['temp'];
+                var currWind = data['wind']['speed'];
+                var currHum = data['main']['humidity'];
+                var currIcon = data['weather'][0]['icon'];
+                var uvLat = data['coord']['lat'];
+                var uvLon = data['coord']['lon'];
+                var currUv = data['main']['uvi'];
+                console.log(currLocation, currTemp, currWind, currHum, uvLat, uvLon, currIcon);
+                displayMainStats(currLocation,currTemp,currWind,currHum,uvLat,uvLon, currIcon);
+            
+            })
+        } else {
+            alert("city not found!");
+        }
+    });
+}
